286e79961d61fe7d02a8eb530b770a6fd9a79788
Josué <Jozsuesantos@gmail.com>
2023-08-13 11:59:31 -0400

    ACT: 100% Responsive

---
+++ js/ScrollReveal.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+    sr.reveal('.contenedor', {
+        duration: 1500,
+        origin: 'right',
+        distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+    sr.reveal('#Yo', {
+        duration: 1000,
+});
+
+window.sr = ScrollReveal();
+    sr.reveal('.presentacion', {
+        duration: 4500,
+});
+
+sr.reveal('.container-fluid', {
+    duration: 1500,
+    origin: 'right',
+    distance: '-100px'
+});
